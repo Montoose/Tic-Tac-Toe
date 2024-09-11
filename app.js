658086742f66d157b83892c52e23159ef15359c6
Matthew <158129347+Montoose@users.noreply.github.com>
2024-09-10 21:33:35 -0500

    Player is able to choose a spot on the board

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const gameBoard = (function () {
                 board[i].push(0);
             }
         }
-        console.log(board);
     }
 
     const reset = () => {
@@ -25,15 +24,67 @@ const gameBoard = (function () {
 })();
 
 function player () {
-    console.log("Placeholder");
+    
+    const makeMove = () => {
+        let playerChoice = Number(prompt("Make your move: (0-8"));
+        switch (playerChoice) {
+            case 0:
+                if (gameBoard.board[0][0] != 1) gameBoard.board[0][0] = 1;
+                break;
+            case 1:
+                if (gameBoard.board[0][1] != 1) gameBoard.board[0][1] = 1;
+                break;
+            case 2:
+                if (gameBoard.board[0][2] != 1) gameBoard.board[0][2] = 1;
+                break;
+            case 3:
+                if (gameBoard.board[1][0] != 1) gameBoard.board[1][0] = 1;
+                break;
+            case 4:
+                if (gameBoard.board[1][1] != 1) gameBoard.board[1][1] = 1;
+                break;
+            case 5:
+                if (gameBoard.board[1][2] != 1) gameBoard.board[1][2] = 1;
+                break;
+            case 6:
+                if (gameBoard.board[2][0] != 1) gameBoard.board[2][0] = 1;
+                break;
+            case 7:
+                if (gameBoard.board[2][1] != 1) gameBoard.board[2][1] = 1;
+                break;
+            case 8:
+                if (gameBoard.board[2][2] != 1) gameBoard.board[2][2] = 1;
+                break;
+        }
+        console.log(gameBoard.board);
+    }
+
+    return {
+        makeMove,
+    }
 }
 
-const computer = (function () {
-    console.log("Placeholder");
-})();
+function computer () {
+    const { makeMove } = player();
 
-const gameLogicController = (function () {
+    return { makeMove };
+}
 
+const gameController = (function () {
+    let activePlayer = true;
+
+    const switchActivePlayer = () => {
+        if (activePlayer = true) activePlayer = false;
+    }
+
+    return {
+        switchActivePlayer,
+    }
 })
 
 gameBoard.setup();
+
+let matthew = new player();
+let ricky = new computer();
+matthew.makeMove();
+ricky.makeMove();
